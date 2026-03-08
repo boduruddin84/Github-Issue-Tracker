fe48054f88e11f97fc8943ef8dae749e9086e4ca
BODURUDDIN <117629880+boduruddin84@users.noreply.github.com>
2026-03-08 12:01:50 +0600

    All Issues Card Added

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,4 @@
+const issuesCount = document.getElementById('issues-count');
 
 async function loadIssues() {
     const res = await fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues")
@@ -6,40 +7,75 @@ async function loadIssues() {
 
 }
 
+const issuesContainer = document.getElementById("Issues-container");
+issuesContainer.innerHTML = " ";
+
 function displayIssues(issues) {
     issues.forEach(issue => {
         const card = document.createElement("div");
+
+        // div.className = "issue-card";
+
+        // if(issue.status === "open"){
+        //     div.style.borderTop = "5px solid green";
+        // }else{
+        //     div.style.borderTop = "5px solid purple";
+        // }
+
         card.innerHTML = `
         <div class="card bg-base-100 w-fit shadow-sm">
           <div class="card-body space-y-3">
             <div class="text-right">
-                <button class="btn rounded-full px-6 py-1.5">High</button>
+                <button class="btn rounded-full px-6 py-1.5">${issue.priority}</button>
             </div>
-            <h3 class="text-[#1F2937] font-semibold text-[14px]">Fix navigation menu on mobile devices</h3>
-            <p class="text-[12px] text-[#64748B]">The navigation menu doesn't collapse properly on mobile devices...</p>
+            <h3 class="text-[#1F2937] font-semibold text-[14px]">${issue.title}</h3>
+            <p class="text-[12px] text-[#64748B]">${issue.description}</p>
             <div class="flex gap-1">
                 <button class="btn rounded-full bg-yellow-400">${issue.labels[0]}</button>
                 <button class="btn rounded-full bg-yellow-400">help wanted</button>
             </div>
             <hr class="text-gray-300">
             <div>
-                <p class="text-[#64748B]">#<span>1</span> by <span>john_doe</span></p>
+                <p class="text-[#64748B]">#<span>${issue.id}</span> by <span>${issue.author}</span></p>
             </div>
             <div>
-                <p class="text-[#64748B]">1/15/2024</p>
+                <p class="text-[#64748B]">${issue.createdAt}</p>
             </div>
           </div>
         </div>
         `;
         issuesContainer.appendChild(card);
     })
+
+    issuesCount.innerText = issues.length;
 }
 
-loadIssues();
 
 
-const issuesContainer = document.getElementById("Issues-container");
-issuesContainer.innerHTML = "";
+async function openIssues(){
+
+const res = await fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues");
+
+const data = await res.json();
+
+const openData = data.data.filter((issue) => issue.status === "open");
+
+
+displayIssues(openData);
+
+}
+
+
+async function closedIssues(){
+
+const res = await fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues");
+const data = await res.json();
+const closeData = data.data.filter((issue) => issue.status === "closed");
+
+displayIssues(closeData);
+}
+
+
 
 
 //       "id": 1,
